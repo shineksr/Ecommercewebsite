79bba453f6faf86525a168345db0f86b69c436c3
shine <shineksr@gmail.com>
2025-06-06 00:26:24 +0530

    log 2

--- server/Models/user.models.js
+++ server/Models/user.models.js
@@ -13,5 +13,66 @@ const userSchema = new mongoose.SchemaType({
     password : {
         type :String,
         required :[true,"Provide password"]
+    },
+    avatar :{
+        type : String,
+        default :  ""
+    },
+    mobile : {
+        type : Number,
+        default : null
+    },
+    refresh_token : {
+        type :String,
+        default :""
+    },
+    verify_email:{
+        type :  Boolean,
+        default :false
+    },
+    last_login_date :{
+        type : Date,
+        default : ""
+    },
+    status :{
+        type : String,
+        enum : ["active","inactive","suspended"],
+        default: "Active"
+    },
+    address_details :[
+        {
+            type :mongoose.Schema.ObjectId,
+            ref : "address"
+        }
+    ],
+      shopping_cart:[
+        {
+            type :mongoose.Schema.ObjectId,
+            ref : "cartProduct"
+        }
+    ],
+     orderHistory:[
+        {
+            type :mongoose.Schema.ObjectId,
+            ref : "order"
+        }
+    ],
+    forgot_password_otp : {
+        type : String,
+        default : null    
+    },
+    forgot_password_otp_expiry : {
+        type : Date,
+        default : ""
+    },
+    role : {
+        type :String,
+        enum : ["ADMIN","USER"],
+        default :"USER"
     }
+},{
+    timestamps: true
 })
+const UserModel =mongoose.model("user",userSchema)
+
+export default  UserModel;
